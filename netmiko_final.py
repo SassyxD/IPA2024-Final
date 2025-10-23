9a781b4d72c790b6bb3b32b29501c174a48a6670
s4ssyxd <xenonhash@gmail.com>
2025-10-23 19:33:05 +0700

    feat: implement Netmiko gigabit_status function

--- netmiko_final.py
+++ netmiko_final.py
@@ -1,12 +1,13 @@
 from netmiko import ConnectHandler
 from pprint import pprint
+import os
 
-device_ip = "<!!!REPLACEME with router IP address!!!>"
+device_ip = os.environ.get("ROUTER_IP", "10.0.15.61")
 username = "admin"
 password = "cisco"
 
 device_params = {
-    "device_type": "<!!!REPLACEME with device type for netmiko!!!>",
+    "device_type": "cisco_ios",
     "ip": device_ip,
     "username": username,
     "password": password,
@@ -19,16 +20,18 @@ def gigabit_status():
         up = 0
         down = 0
         admin_down = 0
-        result = ssh.send_command("<!!!REPLACEME with proper command!!!>", use_textfsm=True)
+        result = ssh.send_command("show ip interface brief", use_textfsm=True)
+        interface_status = []
         for status in result:
-            if <!!!Write code here!!!>:
-                <!!!Write code here!!!>
-                if <!!!Write code here!!!> == "up":
+            if "GigabitEthernet" in status["intf"]:
+                interface_status.append(f"{status['intf']} {status['status']}")
+                if status["status"] == "up":
                     up += 1
-                elif <!!!Write code here!!!> == "down":
+                elif status["status"] == "down":
                     down += 1
-                elif <!!!Write code here!!!> == "administratively down":
+                elif status["status"] == "administratively down":
                     admin_down += 1
-        ans = <!!!Write code here!!!>
+        ans = f"{', '.join(interface_status)} -> {up} up, {down} down, {admin_down} administratively down"
         pprint(ans)
         return ans
+
